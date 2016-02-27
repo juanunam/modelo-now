2e529d7a5af0f6a0d19eae59efd581fc55060854
Michell Ayala <starships@outlook.com>
2016-02-27 11:52:35 -0600

    add: compilation process

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,24 @@
 
 module.exports = (Grunt) => {
 	Grunt.initConfig({
-
+		jade: {
+			debug: {
+				options: { pretty: true },
+				files: {
+					'public/index.html': ['src/views/index.jade']
+				}
+			},
+			release: {
+				options: { pretty: false },
+				files: {
+					'public/index.html': ['src/views/index.jade']
+				}
+			}
+		}
 	});
+
+	Grunt.loadNpmTasks('grunt-contrib-jade');
+	
+	Grunt.registerTask('compile', ['jade:debug']);
 };
 
